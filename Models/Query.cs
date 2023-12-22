9bc251a9495852d20cd4ab8e45dc1baf5388b4b0
Mehran <mr.mehran75@gmail.com>
2023-12-22 23:20:50 +0330

    reading configurations from appsettings.json

---
+++ Models/Query.cs
@@ -0,0 +1,9 @@
+﻿namespace web_crawler_bot.Models
+{
+    public class Query
+    {
+        public string Address { get; set; }
+        public string Selector { get; set; }
+        public string cellTarget { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using web_crawler_bot.Models; // Ensure you have Newtonsoft.Json package installed
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Read the JSON file
+        string jsonFile = File.ReadAllText("appsettings.json");
+
+        // Parse JSON content
+        JObject config = JObject.Parse(jsonFile);
+
+        // Access variables
+        var configurations = config["AppSettings"]!.ToObject<List<Query>>();
+        
+        // Display the values
+        Console.WriteLine($"Variable1: ");
+
+        // Your code using these variables goes here
+    }
+}
